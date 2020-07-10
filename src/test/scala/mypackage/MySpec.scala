7eeb6513ab42e537d77cf1c2c595ad0fa829eb5c
Thomas Smith <thomas@apolloagriculture.com>
2020-07-10 14:09:22 +0200

    Fix bad operand error, introduce type error

--- src/test/scala/mypackage/MySpec.scala
+++ src/test/scala/mypackage/MySpec.scala
@@ -1,14 +1,15 @@
 package mypackage;
 
-import org.mockito.MockitoSugar
-import org.scalatest.FlatSpec
+import org.mockito.{MockitoSugar,ArgumentMatchersSugar}
+import org.scalatest.flatspec.AnyFlatSpec
 
 case class ValueType[A](value: A) extends AnyVal
 
-class MySpec extends FlatSpec with MockitoSugar {
+class MySpec extends AnyFlatSpec with ArgumentMatchersSugar with MockitoSugar {
   val s = mock[MyService]
+  when(s.crash(any[ValueType[Int]])).thenReturn(3)
 }
 
 class MyService {
-  def crash(x: ValueType[Int]): Unit = {}
+  def crash(x: ValueType[Int]): Int = 3
 }
